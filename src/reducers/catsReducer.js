3621b6806402a731ff18efc7cbc08d807b3a0407
eric <contreras.eric18@gmail.com>
2021-10-17 20:57:05 -0400

    commit tree 03af687ee476aa48a8dc279efa2f0d9f1c56cfad

--- src/reducers/catsReducer.js
+++ src/reducers/catsReducer.js
@@ -1,1 +1,27 @@
-// add the catsReducer
+const catsReducer = (
+    state = {
+        cats: [],
+        loading: false, 
+    }, action) =>{
+        switch(action.type){
+
+            default:
+                return state;
+
+            case "LOADING_CATS":
+                return{
+                    ...state, 
+                    cats: [...state.cats],
+                    loading: true,
+                };
+            
+            case "ADD_CATS":
+                return{
+                    ...state, 
+                    cats: action.cats,
+                    loading: false,
+                };
+        }
+    };
+
+    export default catsReducer;
